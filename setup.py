e6888e0952b206e2431b86476367575680c74d0f
Vivek Reddy Karri <vkarri@nvidia.com>
2021-05-05 22:17:45 +0000

    setup.py is updated for test filtering

--- setup.py
+++ setup.py
@@ -5,9 +5,21 @@
 # under scripts/. Consider stop using scripts and use console_scripts instead
 #
 # https://stackoverflow.com/questions/18787036/difference-between-entry-points-console-scripts-and-scripts-in-setup-py
-import fastentrypoints
+import fastentrypoints, sys
 
 from setuptools import setup
+from setuptools.command.test import test as TestCommand
+
+class PyTest(TestCommand):
+    user_options = [("pytest-args=", "a", "Arguments to pass to pytest")]
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.pytest_args = ""
+    def run_tests(self):
+        import shlex
+        import pytest
+        errno = pytest.main(shlex.split(self.pytest_args))
+        sys.exit(errno)
 
 setup(
     name='sonic-utilities',
@@ -191,6 +203,7 @@ setup(
         'Programming Language :: Python :: 3.7',
         'Topic :: Utilities',
     ],
+    cmdclass={"pytest": PyTest},
     keywords='sonic SONiC utilities command line cli CLI',
     test_suite='setup.get_test_suite'
 )
