b5caed2663645cc3d148af35f6ed6505357f4229
Vivek Reddy Karri <vkarri@nvidia.com>
2021-05-20 20:45:38 +0000

    Multi Asic Support and UT's Added
    Signed-off-by: Vivek Reddy Karri <vkarri@nvidia.com>

--- dump/main.py
+++ dump/main.py
@@ -4,8 +4,10 @@ from tabulate import tabulate
 
 sys.path.append(os.path.dirname(__file__))
 import plugins
-from dump.helper import extract_rid, filter_out_dbs
 from dump.redis_match import RedisSource, JsonSource
+from swsscommon.swsscommon import SonicV2Connector
+from utilities_common.multi_asic import multi_asic_ns_choices, multi_asic
+from utilities_common.constants import DEFAULT_NAMESPACE
 
 # Autocompletion Helper
 def get_available_modules(ctx, args, incomplete):
@@ -35,11 +37,19 @@ def dump():
 @click.option('--table', '-t', is_flag=True, default=False, help='Print in tabular format', show_default=True)
 @click.option('--key-map', '-k', is_flag=True, default=False, help="Only fetch the keys matched, don't extract field-value dumps", show_default=True)
 @click.option('--verbose', '-v', is_flag=True, default=False, help="Prints any intermediate output to stdout useful for dev & troubleshooting", show_default=True)
-def state(ctx, module, identifier, db, table, key_map, verbose):
+@click.option('--namespace', '-n', default=DEFAULT_NAMESPACE, type=str, show_default=True, help='Dump the redis-state for this namespace.')  
+def state(ctx, module, identifier, db, table, key_map, verbose, namespace):
     """
     Dump the redis-state of the identifier for the module specified
     """
-
+    if not multi_asic.is_multi_asic() and namespace != DEFAULT_NAMESPACE:
+        click.echo("Namespace option is not valid for a single-ASIC device")
+        ctx.exit()
+    
+    if multi_asic.is_multi_asic() and (namespace != DEFAULT_NAMESPACE and namespace not in multi_asic_ns_choices()):
+        click.echo("Namespace option is not valid. Choose one of {}".format(multi_asic_ns_choices()))
+        ctx.exit()
+    
     if module not in plugins.dump_modules:
         click.echo("No Matching Plugin has been Implemented")
         ctx.exit()
@@ -53,12 +63,13 @@ def state(ctx, module, identifier, db, table, key_map, verbose):
     obj = plugins.dump_modules[module]()
     
     if identifier == "all":
-        ids = obj.get_all_args()
+        ids = obj.get_all_args(namespace)
     else:
         ids = identifier.split(",")
         
     params = {}
     collected_info = {}
+    params['namespace'] = namespace
     for arg in ids: 
         params[plugins.dump_modules[module].ARG_NAME] = arg 
         collected_info[arg] = obj.execute(params)
@@ -66,10 +77,10 @@ def state(ctx, module, identifier, db, table, key_map, verbose):
     if len(db) > 0:
         collected_info = filter_out_dbs(db, collected_info)
     
-    vidtorid = extract_rid(collected_info)
+    vidtorid = extract_rid(collected_info, namespace)
     
     if not key_map:
-        collected_info = populate_fv(collected_info, module)
+        collected_info = populate_fv(collected_info, module, namespace)
     
     for id in vidtorid.keys():
         if  vidtorid[id] and vidtorid:
@@ -79,7 +90,45 @@ def state(ctx, module, identifier, db, table, key_map, verbose):
     
     return 
 
-def populate_fv(info, module):
+def extract_rid(info, ns):
+    r = SonicV2Connector(namespace=ns, host="127.0.0.1")
+    r.connect("ASIC_DB")
+    vidtorid = {}
+    for arg in info.keys():
+        vidtorid[arg] = get_v_r_map(r, info[arg])
+    return vidtorid
+
+def get_v_r_map(r, single_dict):
+    v_r_map = {}
+    asic_obj_ptrn = "ASIC_STATE:.*:oid:0x\w{1,14}"
+    
+    if "ASIC_DB" in single_dict and 'keys' in single_dict["ASIC_DB"]:
+        for redis_key in single_dict["ASIC_DB"]['keys']:
+            if re.match(asic_obj_ptrn, redis_key):
+                matches = re.findall(r"oid:0x\w{1,14}", redis_key)
+                if matches:
+                   vid = matches[0]
+                   v_r_map[vid] =  vid_to_rid(vid, r)
+    return v_r_map
+
+# Get a vid:rid for the input vid
+def vid_to_rid(vid, r):
+    rid = r.get("ASIC_DB", "VIDTORID", vid)
+    if not rid:
+        rid = "Real ID Not Found" 
+    return rid  
+
+# Filter dbs which are not required
+def filter_out_dbs(db_list, collected_info):
+    args_ = list(collected_info.keys())
+    for arg in args_:
+        dbs = list(collected_info[arg].keys())
+        for db in dbs:
+            if db not in db_list:
+                del collected_info[arg][db]
+    return collected_info
+
+def populate_fv(info, module, namespace):
 
     all_dbs = set()
     for id in info.keys():
@@ -90,10 +139,10 @@ def populate_fv(info, module):
     for db_name in all_dbs:
         if db_name is "CONFIG_FILE":
             db_dict[db_name] = JsonSource()
-            db_dict[db_name].connect(plugins.dump_modules[module].CONFIG_FILE)
+            db_dict[db_name].connect(plugins.dump_modules[module].CONFIG_FILE, namespace)
         else:
             db_dict[db_name] = RedisSource()
-            db_dict[db_name].connect(db_name)
+            db_dict[db_name].connect(db_name, namespace)
             
     final_info = {}
     for id in info.keys():
@@ -153,19 +202,19 @@ def print_dump(collected_info, table, module, identifier, key_map):
                     if isinstance(key_obj, dict) and key_obj:
                         key = list(key_obj.keys())[0]
                         values.append([key, get_dict_str(key_obj[key])])
-                total_info += str(tabulate(values, hdrs, tablefmt="pretty"))
+                total_info += str(tabulate(values, hdrs, tablefmt="grid"))
             else:
                 temp = []
                 for key_ in collected_info[ids][db]["keys"]:
                     temp.append([key_])
-                total_info += str(tabulate(temp, headers=["Keys Collected"], tablefmt="pretty"))
+                total_info += str(tabulate(temp, headers=["Keys Collected"], tablefmt="grid"))
 
             total_info += "\n"
             if "vidtorid" in collected_info[ids][db]:
                 table = []
                 for pair in collected_info[ids][db]["vidtorid"].items():
                     table.append(list(pair))
-                total_info +=str(tabulate(table, headers=["vid", "rid"], tablefmt="pretty"))
+                total_info +=str(tabulate(table, headers=["vid", "rid"], tablefmt="grid"))
             final_collection.append([ids, db, total_info])
 
     click.echo(tabulate(final_collection, top_header, tablefmt="grid"))
---
+++ tests/dump_tests/dump_state_test.py
@@ -0,0 +1,245 @@
+import os
+import sys, json
+import pytest
+from unittest import mock, TestCase
+from click.testing import CliRunner
+import dump.main as dump
+from deepdiff import DeepDiff
+from importlib import reload
+from utilities_common.db import Db
+import traceback
+
+def compare_json_output(exp, rec):
+    try:
+        rec_json = json.loads(rec)
+    except Exception as e:
+        assert 0 , "CLI Output is not in JSON Format"    
+    exp_json = json.loads(exp)
+    return DeepDiff(exp_json, rec_json)
+
+table_display_output = '''\
++-------------+-----------+--------------------------------------------------------------------+
+| port_name   | DB_NAME   | DUMP                                                               |
++=============+===========+====================================================================+
+| Ethernet0   | APPL_DB   | +----------------------+-----------------------------------------+ |
+|             |           | | Keys                 | field-value pairs                       | |
+|             |           | +======================+=========================================+ |
+|             |           | | PORT_TABLE:Ethernet0 | +--------------+----------------------+ | |
+|             |           | |                      | | field        | value                | | |
+|             |           | |                      | |--------------+----------------------| | |
+|             |           | |                      | | index        | 0                    | | |
+|             |           | |                      | | lanes        | 0                    | | |
+|             |           | |                      | | alias        | Ethernet0            | | |
+|             |           | |                      | | description  | ARISTA01T2:Ethernet1 | | |
+|             |           | |                      | | speed        | 25000                | | |
+|             |           | |                      | | oper_status  | down                 | | |
+|             |           | |                      | | pfc_asym     | off                  | | |
+|             |           | |                      | | mtu          | 9100                 | | |
+|             |           | |                      | | fec          | rs                   | | |
+|             |           | |                      | | admin_status | up                   | | |
+|             |           | |                      | +--------------+----------------------+ | |
+|             |           | +----------------------+-----------------------------------------+ |
++-------------+-----------+--------------------------------------------------------------------+
+'''
+
+table_display_output_no_filtering= '''\
++-------------+-----------+-----------------------------------------------------------+
+| port_name   | DB_NAME   | DUMP                                                      |
++=============+===========+===========================================================+
+| Ethernet0   | CONFIG_DB | +------------------+                                      |
+|             |           | | Keys Collected   |                                      |
+|             |           | +==================+                                      |
+|             |           | | PORT|Ethernet0   |                                      |
+|             |           | +------------------+                                      |
++-------------+-----------+-----------------------------------------------------------+
+| Ethernet0   | APPL_DB   | +----------------------+                                  |
+|             |           | | Keys Collected       |                                  |
+|             |           | +======================+                                  |
+|             |           | | PORT_TABLE:Ethernet0 |                                  |
+|             |           | +----------------------+                                  |
++-------------+-----------+-----------------------------------------------------------+
+| Ethernet0   | ASIC_DB   | +-------------------------------------------------------+ |
+|             |           | | Keys Collected                                        | |
+|             |           | +=======================================================+ |
+|             |           | | ASIC_STATE:SAI_OBJECT_TYPE_HOSTIF:oid:0xd00000000056d | |
+|             |           | +-------------------------------------------------------+ |
+|             |           | | ASIC_STATE:SAI_OBJECT_TYPE_PORT:oid:0x10000000004a4   | |
+|             |           | +-------------------------------------------------------+ |
+|             |           | +---------------------+---------------------+             |
+|             |           | | vid                 | rid                 |             |
+|             |           | +=====================+=====================+             |
+|             |           | | oid:0xd00000000056d | oid:0xd             |             |
+|             |           | +---------------------+---------------------+             |
+|             |           | | oid:0x10000000004a4 | oid:0x1690000000001 |             |
+|             |           | +---------------------+---------------------+             |
++-------------+-----------+-----------------------------------------------------------+
+| Ethernet0   | STATE_DB  | +----------------------+                                  |
+|             |           | | Keys Collected       |                                  |
+|             |           | +======================+                                  |
+|             |           | | PORT_TABLE|Ethernet0 |                                  |
+|             |           | +----------------------+                                  |
++-------------+-----------+-----------------------------------------------------------+
+'''
+
+class TestDumpState(object):
+    
+    @classmethod
+    def setup_class(cls):
+        print("SETUP")
+        os.environ["UTILITIES_UNIT_TESTING"] = "1"
+    
+    def test_identifier_single(self):
+        runner = CliRunner()
+        result = runner.invoke(dump.state, ["port", "Ethernet0"])
+        expected = ('''{"Ethernet0":{"CONFIG_DB":{"keys":[{"PORT|Ethernet0":{"alias":"etp1","description":"etp1","index":"0","lanes":"25,26,27,28","mtu":"9100","pfc_asym":"off","speed":"40000"}}],''' + 
+        '''"tables_not_found":[]},"APPL_DB":{"keys":[{"PORT_TABLE:Ethernet0":{"index":"0","lanes":"0","alias":"Ethernet0","description":"ARISTA01T2:Ethernet1","speed":"25000","oper_status":"down","pfc_asym":"off","mtu":"9100","fec":"rs","admin_status":"up"}}],''' +
+        '''"tables_not_found":[]},"ASIC_DB":{"keys":[{"ASIC_STATE:SAI_OBJECT_TYPE_HOSTIF:oid:0xd00000000056d":{"SAI_HOSTIF_ATTR_NAME":"Ethernet0","SAI_HOSTIF_ATTR_OBJ_ID":"oid:0x10000000004a4","SAI_HOSTIF_ATTR_OPER_STATUS":"true",'''+
+        '''"SAI_HOSTIF_ATTR_TYPE":"SAI_HOSTIF_TYPE_NETDEV","SAI_HOSTIF_ATTR_VLAN_TAG":"SAI_HOSTIF_VLAN_TAG_STRIP"}},''' +
+        '''{"ASIC_STATE:SAI_OBJECT_TYPE_PORT:oid:0x10000000004a4":{"NULL":"NULL","SAI_PORT_ATTR_ADMIN_STATE":"true","SAI_PORT_ATTR_MTU":"9122","SAI_PORT_ATTR_SPEED":"100000"}}],"tables_not_found":[],"vidtorid":{"oid:0xd00000000056d":"oid:0xd","oid:0x10000000004a4":"oid:0x1690000000001"}},''' +
+        '''"STATE_DB":{"keys":[{"PORT_TABLE|Ethernet0":{"state":"ok"}}],"tables_not_found":[]}}}''')
+#         exc_type, exc_value, exc_tb = result.exc_info
+#         tb = traceback.TracebackException(exc_type, exc_value, exc_tb)
+#         print(''.join(tb.format_exception_only()))
+        assert result.exit_code == 0, "exit code: {}, Exception: {}, Traceback: {}".format(result.exit_code, result.exception, result.exc_info)
+        ddiff = compare_json_output(expected, result.output)
+        assert not ddiff, ddiff
+        
+    def test_identifier_multiple(self):
+        runner = CliRunner()
+        result = runner.invoke(dump.state, ["port", "Ethernet0,Ethernet4"])
+        expected = ('''{"Ethernet0":{"CONFIG_DB":{"keys":[{"PORT|Ethernet0":{"alias":"etp1","description":"etp1","index":"0","lanes":"25,26,27,28",''' +
+        '''"mtu":"9100","pfc_asym":"off","speed":"40000"}}],"tables_not_found":[]},"APPL_DB":{"keys":[{"PORT_TABLE:Ethernet0":{"index":"0",''' +
+        '''"lanes":"0","alias":"Ethernet0","description":"ARISTA01T2:Ethernet1","speed":"25000","oper_status":"down","pfc_asym":"off","mtu":"9100",''' +
+        '''"fec":"rs","admin_status":"up"}}],"tables_not_found":[]},"ASIC_DB":{"keys":[{"ASIC_STATE:SAI_OBJECT_TYPE_HOSTIF:oid:0xd00000000056d":''' +
+        '''{"SAI_HOSTIF_ATTR_NAME":"Ethernet0","SAI_HOSTIF_ATTR_OBJ_ID":"oid:0x10000000004a4","SAI_HOSTIF_ATTR_OPER_STATUS":"true","SAI_HOSTIF_ATTR_TYPE":''' +
+        '''"SAI_HOSTIF_TYPE_NETDEV","SAI_HOSTIF_ATTR_VLAN_TAG":"SAI_HOSTIF_VLAN_TAG_STRIP"}},{"ASIC_STATE:SAI_OBJECT_TYPE_PORT:oid:0x10000000004a4":{"NULL":''' +
+        '''"NULL","SAI_PORT_ATTR_ADMIN_STATE":"true","SAI_PORT_ATTR_MTU":"9122","SAI_PORT_ATTR_SPEED":"100000"}}],"tables_not_found":[],"vidtorid":''' +
+        '''{"oid:0xd00000000056d":"oid:0xd","oid:0x10000000004a4":"oid:0x1690000000001"}},"STATE_DB":{"keys":[{"PORT_TABLE|Ethernet0":{"state":"ok"}}],"tables_not_found":[]}},''' +
+        '''"Ethernet4":{"CONFIG_DB":{"keys":[{"PORT|Ethernet4":{"admin_status":"up","alias":"etp2","description":"Servers0:eth0","index":"1","lanes":"29,30,31,32","mtu":"9100","pfc_asym":"off","speed":"40000"}}],''' +
+        '''"tables_not_found":[]},"APPL_DB":{"keys":[],"tables_not_found":["PORT_TABLE"]},"ASIC_DB":{"keys":[],"tables_not_found"''' +
+        ''':["ASIC_STATE:SAI_OBJECT_TYPE_HOSTIF","ASIC_STATE:SAI_OBJECT_TYPE_PORT"]},"STATE_DB":{"keys":[],"tables_not_found":["PORT_TABLE"]}}}''')
+        assert result.exit_code == 0, "exit code: {}, Exception: {}, Traceback: {}".format(result.exit_code, result.exception, result.exc_info)
+        ddiff = compare_json_output(expected, result.output)
+        assert not ddiff, ddiff
+    
+    def test_option_key_map(self):
+        runner = CliRunner()
+        result = runner.invoke(dump.state, ["port", "Ethernet0", "--key-map"])
+        expected = ('''{"Ethernet0":{"CONFIG_DB":{"keys":["PORT|Ethernet0"],"tables_not_found":[]},"APPL_DB":{"keys":["PORT_TABLE:Ethernet0"],"tables_not_found":[]},'''+
+        '''"ASIC_DB":{"keys":["ASIC_STATE:SAI_OBJECT_TYPE_HOSTIF:oid:0xd00000000056d","ASIC_STATE:SAI_OBJECT_TYPE_PORT:oid:0x10000000004a4"],'''+
+        '''"tables_not_found":[],"vidtorid":{"oid:0xd00000000056d":"oid:0xd","oid:0x10000000004a4":"oid:0x1690000000001"}},'''+
+        '''"STATE_DB":{"keys":["PORT_TABLE|Ethernet0"],"tables_not_found":[]}}}''')
+        assert result.exit_code == 0, "exit code: {}, Exception: {}, Traceback: {}".format(result.exit_code, result.exception, result.exc_info)
+        ddiff = compare_json_output(expected, result.output)
+        assert not ddiff, ddiff
+        
+    def test_option_db_filtering(self):
+        runner = CliRunner()
+        result = runner.invoke(dump.state, ["port", "Ethernet0", "--db", "CONFIG_DB", "--db", "APPL_DB"])
+        expected = ('''{"Ethernet0":{"CONFIG_DB":{"keys":[{"PORT|Ethernet0":{"alias":"etp1","description":"etp1","index":"0","lanes":"25,26,27,28","mtu":"9100",''' +
+        '''"pfc_asym":"off","speed":"40000"}}],"tables_not_found":[]},"APPL_DB":{"keys":[{"PORT_TABLE:Ethernet0":{"index":"0","lanes":"0","alias":"Ethernet0","description":"ARISTA01T2:Ethernet1",''' +
+        '''"speed":"25000","oper_status":"down","pfc_asym":"off","mtu":"9100","fec":"rs","admin_status":"up"}}],"tables_not_found":[]}}}''')
+        assert result.exit_code == 0, "exit code: {}, Exception: {}, Traceback: {}".format(result.exit_code, result.exception, result.exc_info)
+        ddiff = compare_json_output(expected, result.output)
+        assert not ddiff, ddiff
+    
+    def test_option_tabular_display(self):
+        runner = CliRunner()
+        result = runner.invoke(dump.state, ["port", "Ethernet0", "--db", "APPL_DB", "--table"])
+        assert result.exit_code == 0, "exit code: {}, Exception: {}, Traceback: {}".format(result.exit_code, result.exception, result.exc_info)
+        print(result.output)
+        assert table_display_output == result.output
+    
+    def test_option_tabular_display_no_db_filter(self):
+        runner = CliRunner()
+        result = runner.invoke(dump.state, ["port", "Ethernet0", "--table", "--key-map"])
+        assert result.exit_code == 0, "exit code: {}, Exception: {}, Traceback: {}".format(result.exit_code, result.exception, result.exc_info)
+        assert table_display_output_no_filtering == result.output
+    
+    def test_identifier_all_with_filtering(self):
+        runner = CliRunner()
+        expected_entries = []
+        for i in range(0, 125, 4):
+            expected_entries.append("Ethernet" + str(i))
+        result = runner.invoke(dump.state, ["port", "all", "--db", "CONFIG_DB", "--key-map"])
+        try:
+            rec_json = json.loads(result.output)
+        except Exception as e:
+            assert 0 , "CLI Output is not in JSON Format" 
+        ddiff = DeepDiff(set(expected_entries), set(rec_json.keys()))
+        assert not ddiff, "Expected Entries were not recieved when passing all keyword"
+    
+    def test_namespace_single_asic(self):
+        runner = CliRunner()
+        result = runner.invoke(dump.state, ["port", "Ethernet0", "--table", "--key-map", "--namespace", "asic0"])
+        assert result.output == "Namespace option is not valid for a single-ASIC device\n"
+    
+    @classmethod
+    def teardown(cls):
+        print("TEARDOWN")
+        os.environ["UTILITIES_UNIT_TESTING"] = "0"
+    
+class TestDumpStateMultiAsic(object):
+    
+    @classmethod
+    def setup_class(cls):
+        print("SETUP")
+        os.environ["UTILITIES_UNIT_TESTING"] = "2"
+        os.environ["UTILITIES_UNIT_TESTING_TOPOLOGY"] = "multi_asic"
+        from ..mock_tables import mock_multi_asic
+        reload(mock_multi_asic)
+        from ..mock_tables import dbconnector
+        dbconnector.load_namespace_config()
+        
+    def test_default_namespace(self):
+        runner = CliRunner()
+        db = Db()
+        result = runner.invoke(dump.state, ["port", "Ethernet0", "--key-map"], obj=db)
+        expected = ('''{"Ethernet0":{"CONFIG_DB":{"keys":["PORT|Ethernet0"],"tables_not_found":[]},"APPL_DB":{"keys":["PORT_TABLE:Ethernet0"],"tables_not_found":[]},'''+
+        '''"ASIC_DB":{"keys":["ASIC_STATE:SAI_OBJECT_TYPE_HOSTIF:oid:0xd00000000056d","ASIC_STATE:SAI_OBJECT_TYPE_PORT:oid:0x10000000004a4"],'''+
+        '''"tables_not_found":[],"vidtorid":{"oid:0xd00000000056d":"oid:0xd","oid:0x10000000004a4":"oid:0x1690000000001"}},'''+
+        '''"STATE_DB":{"keys":["PORT_TABLE|Ethernet0"],"tables_not_found":[]}}}''')
+        assert result.exit_code == 0, "exit code: {}, Exception: {}, Output: {}".format(result.exit_code, result.exception, result.output)
+        ddiff = compare_json_output(expected, result.output)
+        assert not ddiff, ddiff
+    
+    def test_namespace_asic0(self):
+        runner = CliRunner()
+        db = Db()
+        result = runner.invoke(dump.state, ["port", "Ethernet0", "--namespace", "asic0"], obj=db)
+        expected = ('''{"Ethernet0":{"CONFIG_DB":{"keys":[{"PORT|Ethernet0":{"admin_status":"up","alias":"Ethernet1/1","asic_port_name":'''+
+        '''"Eth0-ASIC0","description":"ARISTA01T2:Ethernet3/1/1","lanes":"33,34,35,36","mtu":"9100","pfc_asym":"off","role":"Ext","speed":"40000"}}],'''+
+        '''"tables_not_found":[]},"APPL_DB":{"keys":[{"PORT_TABLE:Ethernet0":{"lanes":"33,34,35,36","description":"ARISTA01T2:Ethernet3/1/1","pfc_asym":"off",'''+
+        '''"mtu":"9100","alias":"Ethernet1/1","oper_status":"up","admin_status":"up","role":"Ext","speed":"40000","asic_port_name":"Eth0-ASIC0"}}],"tables_not_found":[]},"ASIC_DB":'''+
+        '''{"keys":[],"tables_not_found":["ASIC_STATE:SAI_OBJECT_TYPE_HOSTIF","ASIC_STATE:SAI_OBJECT_TYPE_PORT"]},"STATE_DB":{"keys":[],"tables_not_found":["PORT_TABLE"]}}}''')
+        assert result.exit_code == 0, "exit code: {}, Exception: {}, Output: {}".format(result.exit_code, result.exception, result.output)
+        ddiff = compare_json_output(expected, result.output)
+        assert not ddiff, ddiff
+
+    
+    def test_namespace_asic1(self):
+        runner = CliRunner()
+        db = Db()
+        result = runner.invoke(dump.state, ["port", "Ethernet-BP256", "--namespace", "asic1"], obj=db)
+        expected = ('''{"Ethernet-BP256":{"CONFIG_DB":{"keys":[{"PORT|Ethernet-BP256":{"admin_status":"up","alias":"Ethernet-BP256","asic_port_name":"Eth0-ASIC1","description":"ASIC0:Eth16-ASIC0",'''+
+        '''"lanes":"61,62,63,64","mtu":"9100","pfc_asym":"off","role":"Int","speed":"40000"}}],"tables_not_found":[]},"APPL_DB":{"keys":[{"PORT_TABLE:Ethernet-BP256":{"oper_status":"up",'''+
+        '''"lanes":"61,62,63,64","description":"ASIC0:Eth16-ASIC0","pfc_asym":"off","mtu":"9100","alias":"Ethernet-BP256","admin_status":"up","speed":"40000","asic_port_name":"Eth0-ASIC1"}}],'''+
+        '''"tables_not_found":[]},"ASIC_DB":{"keys":[],"tables_not_found":["ASIC_STATE:SAI_OBJECT_TYPE_HOSTIF","ASIC_STATE:SAI_OBJECT_TYPE_PORT"]},"STATE_DB":{"keys":[],"tables_not_found":["PORT_TABLE"]}}}''')
+        assert result.exit_code == 0, "exit code: {}, Exception: {}, Output: {}".format(result.exit_code, result.exception, result.output)
+        ddiff = compare_json_output(expected, result.output)
+        assert not ddiff, ddiff
+    
+    def test_invalid_namespace(self):
+        runner = CliRunner()
+        db = Db()
+        result = runner.invoke(dump.state, ["port", "Ethernet0", "--namespace", "asic3"], obj=db)
+        assert result.output == "Namespace option is not valid. Choose one of ['asic0', 'asic1']\n"
+    
+
+    def teardown_class(cls):
+        print("TEARDOWN")
+        os.environ["UTILITIES_UNIT_TESTING"] = "0"
+        os.environ["UTILITIES_UNIT_TESTING_TOPOLOGY"] = ""
+    
+    
+    
