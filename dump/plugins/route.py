ab9a98767f8b5ff175bc7568ac33f616cc8691f7
Vivek Reddy Karri <vkarri@nvidia.com>
2021-07-28 06:37:34 +0000

    Module Implementation Started
    Signed-off-by: Vivek Reddy Karri <vkarri@nvidia.com>

--- dump/plugins/route.py
+++ dump/plugins/route.py
@@ -4,7 +4,7 @@ from dump.helper import create_template_dict
 
 class Route(Executor):
     """
-    Debug Dump Plugin for PORT Module
+    Debug Dump Plugin for Route Module
     """
     ARG_NAME = "destination_network"
     
@@ -18,6 +18,28 @@ class Route(Executor):
             
     def execute(self, params):
         self.ret_temp = create_template_dict(dbs=["CONFIG_DB", "APPL_DB", "ASIC_DB"])
-        port_name = params[Route.ARG_NAME]
+        self.dest_net = params[Route.ARG_NAME]
         self.ns = params["namespace"]
+        self.init_route_config_info()
+        self.init_route_appl_info()
         return self.ret_temp
+    
+    def add_to_ret_template(self, table, db, keys, err, add_to_tables_not_found=True):
+        if not err and keys:
+            self.ret_temp[db]["keys"].extend(keys)
+        elif add_to_tables_not_found:
+            self.ret_temp[db]["tables_not_found"].extend([table])
+    
+    def init_route_config_info(self):
+        req = MatchRequest(db="CONFIG_DB", table="STATIC_ROUTE", key_pattern=self.dest_net, ns=self.ns)
+        ret = self.match_engine.fetch(req)
+        self.add_to_ret_template(req.table, req.db, ret["keys"], ret["error"])
+    
+    def init_route_appl_info(self):
+        req = MatchRequest(db="APPL_DB", table="ROUTE_TABLE", key_pattern=self.dest_net, ns=self.ns)
+        ret = self.match_engine.fetch(req)
+        self.add_to_ret_template(req.table, req.db, ret["keys"], ret["error"], True)
+    
+    def init_asic_route_entry_info(self):
+        req = MatchRequest(db="APPL_DB", table="ROUTE_TABLE", key_pattern=self.dest_net, ns=self.ns)
+        pass
