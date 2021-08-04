e68cef479047e5d862957939e12d3c83fe1a27e5
Vivek Reddy Karri <vkarri@nvidia.com>
2021-08-04 21:44:12 +0000

    Route Module files added
    Signed-off-by: Vivek Reddy Karri <vkarri@nvidia.com>

---
+++ dump/plugins/route.py
@@ -0,0 +1,294 @@
+import json, re, copy
+from dump.match_infra import MatchEngine, MatchRequest
+from dump.helper import create_template_dict, verbose_print
+from .executor import Executor
+
+NH = "ASIC_STATE:SAI_OBJECT_TYPE_NEXT_HOP"
+NH_GRP = "ASIC_STATE:SAI_OBJECT_TYPE_NEXT_HOP_GROUP"
+RIF = "ASIC_STATE:SAI_OBJECT_TYPE_ROUTER_INTERFACE"
+CPU_PORT = "ASIC_STATE:SAI_OBJECT_TYPE_PORT"
+
+def get_route_pattern(dest):
+    return "*\"dest\":\"" + dest + "\"*"
+
+def get_vr_oid(asic_route_entry):
+    matches = re.findall("\{.*\}", asic_route_entry)
+    key_dict = {}
+    if matches:
+        try:
+            key_dict = json.loads(matches[0])
+        except Exception as e:
+            pass
+    return key_dict.get("vr", "")
+
+
+class NextHopGroupMatchOptimizer():
+    """
+    A Stateful Wrapper which optimizes the the queries by caching the redis keys.
+    Will be used for these ASIC keys in particular
+    1) SAI_OBJECT_TYPE_NEXT_HOP_GROUP_MEMBER
+    2) SAI_OBJECT_TYPE_NEXT_HOP
+    3) SAI_OBJECT_TYPE_ROUTER_INTERFACE
+    Note: Caches all the kv pairs for a key
+    """
+    def __init__(self, m_engine):
+        self.key_cache = dict() 
+        self.m_engine = m_engine
+    
+    def mutate_request(self, req):
+        fv_requested = []
+        ret_just_keys = req.just_keys
+        fv_requested = copy.deepcopy(req.return_fields)
+        if ret_just_keys:
+            req.just_keys = False
+            req.return_fields = []
+        return req, fv_requested, ret_just_keys
+    
+    def mutate_response(self, ret, fv_requested, ret_just_keys):
+        if not ret_just_keys:
+            return ret
+        new_ret = {"error" : "", "keys" : [], "return_values" : {}}
+        for key_fv in ret["keys"]:
+            if isinstance(key_fv, dict):
+                keys = key_fv.keys()
+                new_ret["keys"].extend(keys)
+                for key in keys:
+                    new_ret["return_values"][key] = {}
+                    for field in fv_requested:
+                        new_ret["return_values"][key][field] = key_fv[key][field]
+        return new_ret            
+            
+    def fill_cache(self, ret):
+        for key_fv in ret["keys"]:
+            keys = key_fv.keys()
+            for key in keys:
+                self.key_cache[key] = key_fv[key]
+    
+    def fetch_from_cache(self, key, req):
+        verbose_print("Cache Hit for Key: {}".format(key))
+        new_ret = {"error" : "", "keys" : [], "return_values" : {}}
+        if not req.just_keys:
+            new_ret["keys"].append(self.key_cache[key])
+        else:
+            new_ret["keys"].append(key)
+            if req.return_fields:
+                new_ret["return_values"][key] = {}
+                for field in req.return_fields:
+                    new_ret["return_values"][key][field] = self.key_cache[key][field]
+        return new_ret
+    
+    def fetch(self, req):
+        key = req.table + ":" + req.key_pattern
+        if key in self.key_cache:
+            return self.fetch_from_cache(key, req)
+        else:
+            req, fv_requested, ret_just_keys = self.mutate_request(req)
+            ret = self.m_engine.fetch(req)
+            if ret["error"]:
+                return ret
+            self.fill_cache(ret)
+            return self.mutate_response(ret, fv_requested, ret_just_keys)
+            
+class Route(Executor):
+    """
+    Debug Dump Plugin for Route Module
+    """
+    ARG_NAME = "destination_network"
+    
+    def __init__(self):
+        self.match_engine = MatchEngine()
+        self.nhgrp_match_engine = NextHopGroupMatchOptimizer(self.match_engine)
+        self.ret_temp = {}
+        self.ns = ''
+        self.dest_net = ''
+        self.nh_id = ''
+        self.nh_type = ''
+          
+    def get_all_args(self, ns=""):
+        req = MatchRequest(db="APPL_DB", table="ROUTE_TABLE", key_pattern="*", ns=self.ns)
+        ret = self.match_engine.fetch(req)
+        all_routes = ret.get("keys", [])
+        return [key[len("ROUTE_TABLE:"):] for key in all_routes]
+            
+    def execute(self, params):
+        self.ret_temp = create_template_dict(dbs=["CONFIG_DB", "APPL_DB", "ASIC_DB"])
+        self.dest_net = params[Route.ARG_NAME]
+        self.ns = params["namespace"]
+        # CONFIG DB
+        if not self.init_route_config_info():
+            del self.ret_temp["CONFIG_DB"]
+        # APPL DB
+        self.init_route_appl_info()
+        # ASIC DB - ROUTE ENTRY
+        self.nh_id, vr = self.init_asic_route_entry_info()
+        # ASIC DB - VIRTUAL ROUTER
+        self.init_asic_vr_info(vr)
+        # ASIC DB - KEYS dependent on NEXT HOP ID
+        self.init_asic_nh()
+        return self.ret_temp
+    
+    def add_to_ret_template(self, table, db, keys, err, add_to_tables_not_found=True):
+        if not err and keys:
+            self.ret_temp[db]["keys"].extend(keys)
+            return keys
+        elif add_to_tables_not_found:
+            self.ret_temp[db]["tables_not_found"].extend([table])
+        return []
+    
+    def init_route_config_info(self):
+        req = MatchRequest(db="CONFIG_DB", table="STATIC_ROUTE", key_pattern=self.dest_net, ns=self.ns)
+        ret = self.match_engine.fetch(req)
+        return self.add_to_ret_template(req.table, req.db, ret["keys"], ret["error"])
+    
+    def init_route_appl_info(self):
+        req = MatchRequest(db="APPL_DB", table="ROUTE_TABLE", key_pattern=self.dest_net, ns=self.ns)
+        ret = self.match_engine.fetch(req)
+        self.add_to_ret_template(req.table, req.db, ret["keys"], ret["error"], True)
+        
+    def init_asic_route_entry_info(self):
+        nh_id_field = "SAI_ROUTE_ENTRY_ATTR_NEXT_HOP_ID"
+        req = MatchRequest(db="ASIC_DB", table="ASIC_STATE:SAI_OBJECT_TYPE_ROUTE_ENTRY", key_pattern=get_route_pattern(self.dest_net), 
+                           ns=self.ns, return_fields=[nh_id_field])
+        ret = self.match_engine.fetch(req)
+        keys = self.add_to_ret_template(req.table, req.db, ret["keys"], ret["error"], True)
+        asic_route_entry = keys[0] if keys else ""
+        vr = get_vr_oid(asic_route_entry)
+        nh_id = ret["return_values"].get(asic_route_entry, {}).get(nh_id_field, "")
+        return nh_id, vr
+
+    def init_asic_vr_info(self, vr):
+        ret = {}
+        if vr:
+            req = MatchRequest(db="ASIC_DB", table="ASIC_STATE:SAI_OBJECT_TYPE_VIRTUAL_ROUTER", key_pattern=vr, ns=self.ns)
+            ret = self.nhgrp_match_engine.fetch(req)
+        self.add_to_ret_template("ASIC_STATE:SAI_OBJECT_TYPE_VIRTUAL_ROUTER", "ASIC_DB", ret.get("keys", []), ret.get("error", ""))
+    
+    def init_asic_nh(self):
+        self.nh_type = self.get_nh_type()
+        nh_ex = NHExtractor.initialize(self)
+        nh_ex.collect()
+        
+    def get_nh_type(self):
+        if not self.nh_id: 
+            return "DROP"
+        ret = self.nhgrp_match_engine.fetch(MatchRequest(db="ASIC_DB", table=NH_GRP, key_pattern=self.nh_id, ns=self.ns))
+        if ret["keys"]: 
+            return NH_GRP
+        ret = self.nhgrp_match_engine.fetch(MatchRequest(db="ASIC_DB", table=NH, key_pattern=self.nh_id, ns=self.ns))
+        if ret["keys"]: 
+            return NH
+        ret = self.nhgrp_match_engine.fetch(MatchRequest(db="ASIC_DB", table=RIF, key_pattern=self.nh_id, ns=self.ns))
+        if ret["keys"]: 
+            return RIF
+        ret = self.nhgrp_match_engine.fetch(MatchRequest(db="ASIC_DB", table=CPU_PORT, key_pattern=self.nh_id, ns=self.ns))
+        if ret["keys"]: 
+            return CPU_PORT
+        return "DROP"            
+    
+class NHExtractor(object):
+    """
+    Base Class for NH_ID Type
+    """
+    @staticmethod
+    def initialize(route_obj):
+        if route_obj.nh_type == NH:
+            return SingleNextHop(route_obj)
+        elif route_obj.nh_type == NH_GRP:
+            return MultipleNextHop(route_obj)
+        elif route_obj.nh_type == RIF:
+            return DirecAttachedRt(route_obj)
+        elif route_obj.nh_type == CPU_PORT:
+            return CPUPort(route_obj)        
+        return NHExtractor(route_obj)
+        
+    def __init__(self, route_obj):
+        self.rt = route_obj
+
+    def collect(self):
+        pass
+    
+    def init_asic_rif_info(self, oid, add_to_tables_not_found=True):
+        ret = {}
+        if oid: 
+            req = MatchRequest(db="ASIC_DB", table=RIF, key_pattern=oid, ns=self.rt.ns)
+            ret = self.rt.nhgrp_match_engine.fetch(req)
+        return self.rt.add_to_ret_template(RIF, "ASIC_DB", ret.get("keys", []), ret.get("error", ""), add_to_tables_not_found)
+    
+    def init_asic_next_hop_info(self, oid, add_to_tables_not_found=True):
+        ret = {}
+        if oid:
+            req = MatchRequest(db="ASIC_DB", table=NH, key_pattern=oid, ns=self.rt.ns,
+                               return_fields=["SAI_NEXT_HOP_ATTR_ROUTER_INTERFACE_ID"])
+            ret = self.rt.nhgrp_match_engine.fetch(req)
+        keys = self.rt.add_to_ret_template(NH, "ASIC_DB", ret.get("keys", []), ret.get("error", ""), add_to_tables_not_found)
+        nh_key = keys[0] if keys else ""
+        return ret.get("return_values", {}).get(nh_key, {}).get("SAI_NEXT_HOP_ATTR_ROUTER_INTERFACE_ID", "")    
+        
+class CPUPort(NHExtractor):
+    def collect(self):
+        req = MatchRequest(db="ASIC_DB", table=CPU_PORT, key_pattern=self.rt.nh_id, ns=self.rt.ns)
+        ret = self.rt.nhgrp_match_engine.fetch(req)
+        self.rt.add_to_ret_template(req.table, req.db, ret["keys"], ret["error"], True)
+
+class DirecAttachedRt(NHExtractor):
+    def collect(self):
+        self.init_asic_rif_info(self.rt.nh_id) 
+        
+class SingleNextHop(NHExtractor):
+    def collect(self):
+        rif_oid = self.init_asic_next_hop_info(self.rt.nh_id)
+        self.init_asic_rif_info(rif_oid)      
+    
+class MultipleNextHop(NHExtractor):
+    def collect(self):
+        self.init_asic_nh_group_info(self.rt.nh_id)
+        nh_oids = self.init_asic_nh_group_members_info(self.rt.nh_id)
+        rif_oids = self.init_asic_next_hops_info(nh_oids)
+        self.init_asic_rifs_info(rif_oids)
+        
+    def init_asic_nh_group_info(self, oid):
+        ret = {}
+        if oid:
+            req = MatchRequest(db="ASIC_DB", table=NH_GRP, key_pattern=oid, ns=self.rt.ns)
+            ret = self.rt.nhgrp_match_engine.fetch(req)
+        self.rt.add_to_ret_template(NH_GRP, "ASIC_DB", ret.get("keys", []), ret.get("error", ""))
+        
+    def init_asic_nh_group_members_info(self, oid):
+        ret = {}
+        if oid:
+            req = MatchRequest(db="ASIC_DB", table="ASIC_STATE:SAI_OBJECT_TYPE_NEXT_HOP_GROUP_MEMBER", 
+                               field="SAI_NEXT_HOP_GROUP_MEMBER_ATTR_NEXT_HOP_GROUP_ID", value=oid, ns=self.rt.ns, 
+                               return_fields=["SAI_NEXT_HOP_GROUP_MEMBER_ATTR_NEXT_HOP_ID"])
+            ret = self.rt.nhgrp_match_engine.fetch(req)    
+        keys = self.rt.add_to_ret_template("ASIC_STATE:SAI_OBJECT_TYPE_NEXT_HOP_GROUP_MEMBER", "ASIC_DB", 
+                                           ret.get("keys", []), ret.get("error", ""), False)
+        if not keys:
+            self.rt.ret_temp["ASIC_DB"]["tables_not_found"].append("ASIC_STATE:SAI_OBJECT_TYPE_NEXT_HOP_GROUP_MEMBER")
+        return [ret.get("return_values", {}).get(key, {}).get("SAI_NEXT_HOP_GROUP_MEMBER_ATTR_NEXT_HOP_ID", "") for key in keys]
+    
+    def init_asic_next_hops_info(self, nh_oids):
+        rif_oids = []
+        for oid in nh_oids:
+            rif_oid = self.init_asic_next_hop_info(oid, False)
+            if rif_oid:
+                rif_oids.append(rif_oid)
+        if not rif_oids:
+            self.rt.ret_temp["ASIC_DB"]["tables_not_found"].append("ASIC_STATE:SAI_OBJECT_TYPE_NEXT_HOP")
+        return rif_oids
+    
+    def init_asic_rifs_info(self, rif_oids):
+        nothing_found = True
+        for oid in rif_oids:
+            if self.init_asic_rif_info(oid, False):
+                nothing_found = False
+        if nothing_found:
+            self.rt.ret_temp["ASIC_DB"]["tables_not_found"].append("ASIC_STATE:SAI_OBJECT_TYPE_ROUTER_INTERFACE")
+
+  
+        
+        
+        
+        
+        
+        
+        
---
+++ tests/dump_tests/module_tests/route_test.py
@@ -0,0 +1,229 @@
+import os
+import unittest
+import pytest
+from deepdiff import DeepDiff
+from mock import patch
+from dump.helper import create_template_dict
+from dump.plugins.route import Route 
+
+from .mock_sonicv2connector import MockSonicV2Connector
+
+module_tests_path = os.path.dirname(__file__)
+dump_tests_path = os.path.join(module_tests_path, "../")
+tests_path = os.path.join(dump_tests_path, "../")
+dump_test_input = os.path.join(tests_path, "dump_input")
+
+# Location for dedicated db's used for UT
+Route_files_path = os.path.join(dump_test_input, "route")
+
+dedicated_dbs = {}
+dedicated_dbs['CONFIG_DB'] = os.path.join(Route_files_path, "config_db.json") 
+dedicated_dbs['APPL_DB'] = os.path.join(Route_files_path, "appl_db.json") 
+dedicated_dbs['ASIC_DB'] = os.path.join(Route_files_path, "asic_db.json")
+
+def mock_connector(namespace, use_unix_socket_path=True):
+    return MockSonicV2Connector(dedicated_dbs, namespace=namespace, use_unix_socket_path=use_unix_socket_path)
+
+@pytest.fixture(scope="module", autouse=True)
+def verbosity_setup():
+    print("SETUP")
+    os.environ["VERBOSE"] = "1"
+    yield
+    print("TEARDOWN")
+    os.environ["VERBOSE"] = "0"
+
+def get_asic_route_key(dest):
+    return "ASIC_STATE:SAI_OBJECT_TYPE_ROUTE_ENTRY:{\"dest\":\"" + dest + \
+            "\",\"switch_id\":\"oid:0x21000000000000\",\"vr\":\"oid:0x3000000000002\"}"
+
+@patch("dump.match_infra.SonicV2Connector", mock_connector)
+class TestRouteModule(unittest.TestCase):
+    def test_static_route(self):
+        """
+        Scenario: Fetch the Keys related to a Static Route from CONF, APPL & ASIC DB's
+                  1) CONF & APPL are straightforward
+                  2) SAI_ROUTE_ENTRY_ATTR_NEXT_HOP_ID = SAI_OBJECT_TYPE_NEXT_HOP here
+                  For More details about SAI_ROUTE_ENTRY_ATTR_NEXT_HOP_ID, check the SAI header in sairoute.h
+        """
+        params = {Route.ARG_NAME : "20.0.0.0/24", "namespace" : ""}
+        m_route = Route()
+        returned = m_route.execute(params)
+        expect = create_template_dict(dbs=["CONFIG_DB", "APPL_DB", "ASIC_DB"])
+        expect["CONFIG_DB"]["keys"].append("STATIC_ROUTE|20.0.0.0/24")
+        expect["APPL_DB"]["keys"].append("ROUTE_TABLE:20.0.0.0/24")
+        expect["ASIC_DB"]["keys"].append(get_asic_route_key("20.0.0.0/24"))
+        expect["ASIC_DB"]["keys"].append("ASIC_STATE:SAI_OBJECT_TYPE_NEXT_HOP:oid:0x40000000002e7")
+        expect["ASIC_DB"]["keys"].append("ASIC_STATE:SAI_OBJECT_TYPE_ROUTER_INTERFACE:oid:0x60000000002cd")
+        expect["ASIC_DB"]["keys"].append("ASIC_STATE:SAI_OBJECT_TYPE_VIRTUAL_ROUTER:oid:0x3000000000002")
+        ddiff = DeepDiff(returned, expect, ignore_order=True)
+        assert not ddiff, ddiff
+        
+    def test_ip2me_route(self):
+        """
+        Scenario: Fetch the keys related to a ip2me route from APPL & ASIC DB.
+                  1) CONF DB doesn't have a ip2me route entry unlike a static route.
+                  2) APPL is straightforward
+                  3) SAI_ROUTE_ENTRY_ATTR_NEXT_HOP_ID = SAI_OBJECT_TYPE_PORT (CPU Port)
+                  4) Thus, no SAI_OBJECT_TYPE_ROUTER_INTERFACE entry for this route
+                  For More details about SAI_ROUTE_ENTRY_ATTR_NEXT_HOP_ID, check the SAI header in sairoute.h 
+        """
+        params = {Route.ARG_NAME : "fe80::/64", "namespace" : ""}
+        m_route = Route()
+        returned = m_route.execute(params)
+        expect = create_template_dict(dbs=["APPL_DB", "ASIC_DB"])
+        expect["APPL_DB"]["keys"].append("ROUTE_TABLE:fe80::/64")
+        expect["ASIC_DB"]["keys"].append(get_asic_route_key("fe80::/64"))
+        expect["ASIC_DB"]["keys"].append("ASIC_STATE:SAI_OBJECT_TYPE_PORT:oid:0x1000000000001")
+        expect["ASIC_DB"]["keys"].append("ASIC_STATE:SAI_OBJECT_TYPE_VIRTUAL_ROUTER:oid:0x3000000000002")
+        ddiff = DeepDiff(returned, expect, ignore_order=True)
+        assert not ddiff, ddiff
+    
+    def test_directly_connected_route(self):
+        """
+        Scenario: Fetch the keys related to a directly connected route from APPL & ASIC DB.
+                  1) CONF DB doesn't have this route entry
+                  2) APPL is straightforward
+                  3) SAI_ROUTE_ENTRY_ATTR_NEXT_HOP_ID = SAI_OBJECT_TYPE_ROUTER_INTERFACE
+                  For More details about SAI_ROUTE_ENTRY_ATTR_NEXT_HOP_ID, check the SAI header in sairoute.h 
+        """
+        params = {Route.ARG_NAME : "1.1.1.0/24", "namespace" : ""}
+        m_route = Route()
+        returned = m_route.execute(params)
+        expect = create_template_dict(dbs=["APPL_DB", "ASIC_DB"])
+        expect["APPL_DB"]["keys"].append("ROUTE_TABLE:1.1.1.0/24")
+        expect["ASIC_DB"]["keys"].append(get_asic_route_key("1.1.1.0/24"))
+        expect["ASIC_DB"]["keys"].append("ASIC_STATE:SAI_OBJECT_TYPE_ROUTER_INTERFACE:oid:0x60000000002cd")
+        expect["ASIC_DB"]["keys"].append("ASIC_STATE:SAI_OBJECT_TYPE_VIRTUAL_ROUTER:oid:0x3000000000002")
+        ddiff = DeepDiff(returned, expect, ignore_order=True)
+        assert not ddiff, ddiff
+    
+    def test_route_with_next_hop(self):
+        """
+        Scenario: Fetch the keys related to a route with next hop.
+                  1) CONF DB doesn't have this route entry
+                  2) APPL is straightforward
+                  3) SAI_ROUTE_ENTRY_ATTR_NEXT_HOP_ID = SAI_OBJECT_TYPE_NEXT_HOP
+                  For More details about SAI_ROUTE_ENTRY_ATTR_NEXT_HOP_ID, check the SAI header in sairoute.h 
+        """
+        params = {Route.ARG_NAME : "10.212.0.0/16", "namespace" : ""}
+        m_route = Route()
+        returned = m_route.execute(params)
+        expect = create_template_dict(dbs=["APPL_DB", "ASIC_DB"])
+        expect["APPL_DB"]["keys"].append("ROUTE_TABLE:10.212.0.0/16")
+        expect["ASIC_DB"]["keys"].append(get_asic_route_key("10.212.0.0/16"))
+        expect["ASIC_DB"]["keys"].append("ASIC_STATE:SAI_OBJECT_TYPE_NEXT_HOP:oid:0x40000000002e7")
+        expect["ASIC_DB"]["keys"].append("ASIC_STATE:SAI_OBJECT_TYPE_ROUTER_INTERFACE:oid:0x60000000002cd")
+        expect["ASIC_DB"]["keys"].append("ASIC_STATE:SAI_OBJECT_TYPE_VIRTUAL_ROUTER:oid:0x3000000000002")
+        ddiff = DeepDiff(returned, expect, ignore_order=True)
+        assert not ddiff, ddiff
+
+    def test_all_args(self):
+        """
+        Scenario: Verify Whether the get_all_args method is working as expected
+        """
+        m_route = Route()
+        returned = m_route.get_all_args("")
+        expect = ["1.1.1.0/24", "10.1.0.32", "10.212.0.0/16", "20.0.0.0/24", "192.168.0.10/22",
+                  "fe80::/64", "20c0:e6e0:0:80::/64", "192.168.0.4/24"]
+        ddiff = DeepDiff(expect, returned, ignore_order=True)
+        assert not ddiff, ddiff
+    
+    def test_no_next_hop_id(self):
+        """
+        Scenario: Fetch the keys related to a route with no next hop id
+                  1) CONF DB doesn't have this route entry
+                  2) APPL is straightforward
+                  3) SAI_ROUTE_ENTRY_ATTR_NEXT_HOP_ID = EMPTY
+                  For More details about SAI_ROUTE_ENTRY_ATTR_NEXT_HOP_ID, check the SAI header in sairoute.h 
+        """
+        params = {Route.ARG_NAME : "0.0.0.0/0", "namespace" : ""}
+        m_route = Route()
+        returned = m_route.execute(params)
+        expect = create_template_dict(dbs=["APPL_DB", "ASIC_DB"])
+        expect["APPL_DB"]["tables_not_found"].append("ROUTE_TABLE")
+        expect["ASIC_DB"]["keys"].append(get_asic_route_key("0.0.0.0/0"))
+        expect["ASIC_DB"]["keys"].append("ASIC_STATE:SAI_OBJECT_TYPE_VIRTUAL_ROUTER:oid:0x3000000000002")
+        ddiff = DeepDiff(returned, expect, ignore_order=True) 
+        assert not ddiff, ddiff
+    
+    def get_asic_nh_group_expected(self, asic_route_key):
+        expect = []
+        expect.append(asic_route_key)
+        exp_nh_group = "ASIC_STATE:SAI_OBJECT_TYPE_NEXT_HOP_GROUP:oid:0x5000000000689"
+        exp_nh_group_mem = ["ASIC_STATE:SAI_OBJECT_TYPE_NEXT_HOP_GROUP_MEMBER:oid:0x2d00000000068a",
+                            "ASIC_STATE:SAI_OBJECT_TYPE_NEXT_HOP_GROUP_MEMBER:oid:0x2d00000000068b",
+                            "ASIC_STATE:SAI_OBJECT_TYPE_NEXT_HOP_GROUP_MEMBER:oid:0x2d00000000068c",
+                            "ASIC_STATE:SAI_OBJECT_TYPE_NEXT_HOP_GROUP_MEMBER:oid:0x2d00000000068d"]
+        exp_nh = ["ASIC_STATE:SAI_OBJECT_TYPE_NEXT_HOP:oid:0x400000000066f", 
+                  "ASIC_STATE:SAI_OBJECT_TYPE_NEXT_HOP:oid:0x400000000067f",
+                  "ASIC_STATE:SAI_OBJECT_TYPE_NEXT_HOP:oid:0x4000000000665",
+                  "ASIC_STATE:SAI_OBJECT_TYPE_NEXT_HOP:oid:0x4000000000667"]
+        exp_rif = ["ASIC_STATE:SAI_OBJECT_TYPE_ROUTER_INTERFACE:oid:0x60000000005c6",
+                   "ASIC_STATE:SAI_OBJECT_TYPE_ROUTER_INTERFACE:oid:0x60000000005c7",
+                   "ASIC_STATE:SAI_OBJECT_TYPE_ROUTER_INTERFACE:oid:0x60000000005c8",
+                   "ASIC_STATE:SAI_OBJECT_TYPE_ROUTER_INTERFACE:oid:0x60000000005c9"]
+        expect.append(exp_nh_group)
+        expect.extend(exp_nh_group_mem)
+        expect.extend(exp_nh)
+        expect.extend(exp_rif)
+        expect.append("ASIC_STATE:SAI_OBJECT_TYPE_VIRTUAL_ROUTER:oid:0x3000000000002")
+        return expect
+    
+    def test_route_with_next_hop_group(self):
+        """
+        Scenario: Fetch the keys related to a route with multiple next hops.
+                  1) CONF DB doesn't have this route entry
+                  2) APPL is straightforward
+                  3) SAI_ROUTE_ENTRY_ATTR_NEXT_HOP_ID = SAI_OBJECT_TYPE_NEXT_HOP_GROUP
+                  For More details about SAI_ROUTE_ENTRY_ATTR_NEXT_HOP_ID, check the SAI header in sairoute.h 
+        """
+        params = {Route.ARG_NAME : "20c0:e6e0:0:80::/64", "namespace" : ""}
+        m_route = Route()
+        returned = m_route.execute(params)
+        expect = create_template_dict(dbs=["APPL_DB", "ASIC_DB"])
+        expect["APPL_DB"]["keys"].append("ROUTE_TABLE:20c0:e6e0:0:80::/64")
+        expect["ASIC_DB"]["keys"].extend(self.get_asic_nh_group_expected(get_asic_route_key("20c0:e6e0:0:80::/64")))
+        ddiff = DeepDiff(returned, expect, ignore_order=True)
+        assert not ddiff, ddiff
+    
+    def test_caching_redis_keys(self):
+        """
+        Scenario: Test the caching mechanism which reduces number of redis calls
+        """
+        m_route = Route()
+        params = {Route.ARG_NAME : "20c0:e6e0:0:80::/64", "namespace" : ""}
+        returned = m_route.execute(params)
+        expect = create_template_dict(dbs=["APPL_DB", "ASIC_DB"])
+        expect["APPL_DB"]["keys"].append("ROUTE_TABLE:20c0:e6e0:0:80::/64")
+        expect["ASIC_DB"]["keys"].extend(self.get_asic_nh_group_expected(get_asic_route_key("20c0:e6e0:0:80::/64")))
+        ddiff = DeepDiff(returned, expect, ignore_order=True)
+    
+        params = {Route.ARG_NAME : "192.168.0.4/24", "namespace" : ""}
+        returned = m_route.execute(params)
+        expect = create_template_dict(dbs=["APPL_DB", "ASIC_DB"])
+        expect["APPL_DB"]["keys"].append("ROUTE_TABLE:192.168.0.4/24")
+        expect["ASIC_DB"]["keys"].extend(self.get_asic_nh_group_expected(get_asic_route_key("192.168.0.4/24")))
+        ddiff = DeepDiff(returned, expect, ignore_order=True)
+        assert not ddiff, ddiff
+        
+        params = {Route.ARG_NAME : "192.168.0.10/22", "namespace" : ""}
+        returned = m_route.execute(params)
+        expect = create_template_dict(dbs=["APPL_DB", "ASIC_DB"])
+        expect["APPL_DB"]["keys"].append("ROUTE_TABLE:192.168.0.10/22")
+        expect["ASIC_DB"]["keys"].extend(self.get_asic_nh_group_expected(get_asic_route_key("192.168.0.10/22")))
+        ddiff = DeepDiff(returned, expect, ignore_order=True)
+        assert not ddiff, ddiff
+    
+    def test_no_route_entry(self):
+        """
+        Scenario: Fetch the keys related to a non-exitent route
+        """
+        params = {Route.ARG_NAME : "192.168.19.45/28", "namespace" : ""}
+        m_route = Route()
+        returned = m_route.execute(params)
+        expect = create_template_dict(dbs=["APPL_DB", "ASIC_DB"])
+        expect["APPL_DB"]["tables_not_found"].append("ROUTE_TABLE")
+        expect["ASIC_DB"]["tables_not_found"].append("ASIC_STATE:SAI_OBJECT_TYPE_ROUTE_ENTRY")
+        expect["ASIC_DB"]["tables_not_found"].append("ASIC_STATE:SAI_OBJECT_TYPE_VIRTUAL_ROUTER")
+        ddiff = DeepDiff(returned, expect, ignore_order=True) 
+        assert not ddiff, ddiff
