c89f15f43da792e0327061ec26b9d3b794c53fa9
Vivek Reddy Karri <vkarri@nvidia.com>
2021-08-09 20:08:20 +0000

    Added original Setup.py
    Signed-off-by: Vivek Reddy Karri <vkarri@nvidia.com>

--- setup.py
+++ setup.py
@@ -5,21 +5,9 @@
 # under scripts/. Consider stop using scripts and use console_scripts instead
 #
 # https://stackoverflow.com/questions/18787036/difference-between-entry-points-console-scripts-and-scripts-in-setup-py
-import fastentrypoints, sys
+import fastentrypoints
 
 from setuptools import setup
-from setuptools.command.test import test as TestCommand
-
-class PyTest(TestCommand):
-    user_options = [("pytest-args=", "a", "Arguments to pass to pytest")]
-    def initialize_options(self):
-        TestCommand.initialize_options(self)
-        self.pytest_args = ""
-    def run_tests(self):
-        import shlex
-        import pytest
-        errno = pytest.main(shlex.split(self.pytest_args))
-        sys.exit(errno)
 
 setup(
     name='sonic-utilities',
@@ -43,8 +31,6 @@ setup(
         'crm',
         'debug',
         'generic_config_updater',
-        'dump',
-        'dump.plugins',
         'pfcwd',
         'sfputil',
         'ssdutil',
@@ -85,8 +71,7 @@ setup(
                   'filter_fdb_input/*',
                   'pfcwd_input/*',
                   'wm_input/*',
-                  'ecn_input/*',
-                  'dump_input/*']
+                  'ecn_input/*']
     },
     scripts=[
         'scripts/aclshow',
@@ -145,7 +130,8 @@ setup(
         'scripts/watermarkstat',
         'scripts/watermarkcfg',
         'scripts/sonic-kdump-config',
-        'scripts/centralize_database'
+        'scripts/centralize_database',
+        'scripts/null_route_helper'
     ],
     entry_points={
         'console_scripts': [
@@ -156,7 +142,6 @@ setup(
             'counterpoll = counterpoll.main:cli',
             'crm = crm.main:cli',
             'debug = debug.main:cli',
-            'dump = dump.main:dump',
             'filter_fdb_entries = fdbutil.filter_fdb_entries:main',
             'pfcwd = pfcwd.main:cli',
             'sfputil = sfputil.main:cli',
@@ -232,6 +217,5 @@ setup(
         'Topic :: Utilities',
     ],
     keywords='sonic SONiC utilities command line cli CLI',
-    cmdclass={"pytest": PyTest},
     test_suite='setup.get_test_suite'
 )
