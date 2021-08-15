9cdf76efb2b99b18f104261fdf52b95f49eb8a90
Vivek Reddy Karri <vkarri@nvidia.com>
2021-08-15 01:31:11 +0000

    PEP-8 issues handled
    Signed-off-by: Vivek Reddy Karri <vkarri@nvidia.com>

--- dump/main.py
+++ dump/main.py
@@ -1,16 +1,19 @@
 import os,sys,json,re
 import click
 from tabulate import tabulate
+from sonic_py_common import multi_asic
+from utilities_common.constants import DEFAULT_NAMESPACE
+from utilities_common.general import load_db_config
 
 sys.path.append(os.path.dirname(__file__))
 import plugins
 from dump.match_infra import RedisSource, JsonSource
-from sonic_py_common import multi_asic
-from utilities_common.constants import DEFAULT_NAMESPACE
+
 
 # Autocompletion Helper
 def get_available_modules(ctx, args, incomplete):
     return [k for k in plugins.dump_modules.keys() if incomplete in k]
+
 
 # Display Modules Callback
 def show_modules(ctx, param, value):
@@ -22,21 +25,29 @@ def show_modules(ctx, param, value):
         display.append((mod, plugins.dump_modules[mod].ARG_NAME))
     click.echo(tabulate(display, header))
     ctx.exit()
-             
+
+
 @click.group()
 def dump():
     pass
 
+
 @dump.command()
 @click.pass_context
 @click.argument('module', required=True, type=str, autocompletion=get_available_modules)
-@click.argument('identifier', required=True, type=str) 
-@click.option('--show', '-s', is_flag=True, default=False, help='Display Modules Available', is_eager=True, expose_value=False, callback=show_modules)
-@click.option('--db', '-d', multiple=True, help='Only dump from these Databases or the CONFIG_FILE')
-@click.option('--table', '-t', is_flag=True, default=False, help='Print in tabular format', show_default=True)
-@click.option('--key-map', '-k', is_flag=True, default=False, help="Only fetch the keys matched, don't extract field-value dumps", show_default=True)
-@click.option('--verbose', '-v', is_flag=True, default=False, help="Prints any intermediate output to stdout useful for dev & troubleshooting", show_default=True)
-@click.option('--namespace', '-n', default=DEFAULT_NAMESPACE, type=str, show_default=True, help='Dump the redis-state for this namespace.')  
+@click.argument('identifier', required=True, type=str)
+@click.option('--show', '-s', is_flag=True, default=False, expose_value=False,
+              callback=show_modules, help='Display Modules Available', is_eager=True)
+@click.option('--db', '-d', multiple=True,
+              help='Only dump from these Databases or the CONFIG_FILE')
+@click.option('--table', '-t', is_flag=True, default=False,
+              help='Print in tabular format', show_default=True)
+@click.option('--key-map', '-k', is_flag=True, default=False, show_default=True,
+              help="Only fetch the keys matched, don't extract field-value dumps")
+@click.option('--verbose', '-v', is_flag=True, default=False, show_default=True,
+              help="Prints any intermediate output to stdout useful for dev & troubleshooting")
+@click.option('--namespace', '-n', default=DEFAULT_NAMESPACE, type=str,
+              show_default=True, help='Dump the redis-state for this namespace.')
 def state(ctx, module, identifier, db, table, key_map, verbose, namespace):
     """
     Dump the current state of the identifier for the specified module from Redis DB or CONFIG_FILE
@@ -44,49 +55,53 @@ def state(ctx, module, identifier, db, table, key_map, verbose, namespace):
     if not multi_asic.is_multi_asic() and namespace != DEFAULT_NAMESPACE:
         click.echo("Namespace option is not valid for a single-ASIC device")
         ctx.exit()
-    
+
+    # Load database config files
+    load_db_config()
+
     if multi_asic.is_multi_asic() and (namespace != DEFAULT_NAMESPACE and namespace not in multi_asic.get_namespace_list()):
         click.echo("Namespace option is not valid. Choose one of {}".format(multi_asic.get_namespace_list()))
         ctx.exit()
-    
+
     if module not in plugins.dump_modules:
         click.echo("No Matching Plugin has been Implemented")
         ctx.exit()
-    
+
     if verbose:
         os.environ["VERBOSE"] = "1"
     else:
         os.environ["VERBOSE"] = "0"
-    
+
     ctx.module = module
     obj = plugins.dump_modules[module]()
-    
+
     if identifier == "all":
         ids = obj.get_all_args(namespace)
     else:
         ids = identifier.split(",")
-        
+
     params = {}
     collected_info = {}
     params['namespace'] = namespace
     for arg in ids: 
         params[plugins.dump_modules[module].ARG_NAME] = arg 
         collected_info[arg] = obj.execute(params)
-        
+
     if len(db) > 0:
         collected_info = filter_out_dbs(db, collected_info)
-    
+
     vidtorid = extract_rid(collected_info, namespace)
-    
+
     if not key_map:
         collected_info = populate_fv(collected_info, module, namespace)
-    
+
     for id in vidtorid.keys():
         collected_info[id]["ASIC_DB"]["vidtorid"] = vidtorid[id]
-         
+
     print_dump(collected_info, table, module, identifier, key_map)
-    
-    return 
+
+    return
+
 
 def extract_rid(info, ns):
     r = RedisSource()
@@ -98,20 +113,22 @@ def extract_rid(info, ns):
             vidtorid[arg] = mp
     return vidtorid
 
+
 def get_v_r_map(r, single_dict):
     v_r_map = {}
     asic_obj_ptrn = "ASIC_STATE:.*:oid:0x\w{1,14}"
-    
-    if "ASIC_DB" in single_dict and 'keys' in single_dict["ASIC_DB"]:
-        for redis_key in single_dict["ASIC_DB"]['keys']:
+
+    if "ASIC_DB" in single_dict and "keys" in single_dict["ASIC_DB"]:
+        for redis_key in single_dict["ASIC_DB"]["keys"]:
             if re.match(asic_obj_ptrn, redis_key):
                 matches = re.findall(r"oid:0x\w{1,14}", redis_key)
                 if matches:
-                   vid = matches[0]
-                   v_r_map[vid] =  r.hget("ASIC_DB", "VIDTORID", vid)
-                   if not v_r_map[vid]:
-                       v_r_map[vid] = "Real ID Not Found"
+                    vid = matches[0]
+                    v_r_map[vid] = r.hget("ASIC_DB", "VIDTORID", vid)
+                    if not v_r_map[vid]:
+                        v_r_map[vid] = "Real ID Not Found"
     return v_r_map
+
 
 # Filter dbs which are not required
 def filter_out_dbs(db_list, collected_info):
@@ -123,14 +140,14 @@ def filter_out_dbs(db_list, collected_info):
                 del collected_info[arg][db]
     return collected_info
 
-def populate_fv(info, module, namespace):
 
+def populate_fv(info, module, namespace):
     all_dbs = set()
     for id in info.keys():
         for db_name in info[id].keys():
             all_dbs.add(db_name)
-            
-    db_dict = {}  
+
+    db_dict = {}
     for db_name in all_dbs:
         if db_name is "CONFIG_FILE":
             db_dict[db_name] = JsonSource()
@@ -138,7 +155,7 @@ def populate_fv(info, module, namespace):
         else:
             db_dict[db_name] = RedisSource()
             db_dict[db_name].connect(db_name, namespace)
-            
+
     final_info = {}
     for id in info.keys():
         final_info[id] = {}
@@ -148,8 +165,9 @@ def populate_fv(info, module, namespace):
             final_info[id][db_name]["tables_not_found"] = info[id][db_name]["tables_not_found"]
             for key in info[id][db_name]["keys"]:
                 final_info[id][db_name]["keys"].append({key : db_dict[db_name].get(db_name, key)})
-                 
+
     return final_info
+
 
 def get_dict_str(key_obj):
     table = []
@@ -157,15 +175,7 @@ def get_dict_str(key_obj):
         table.append(list(pair))
     return tabulate(table, headers=["field", "value"], tablefmt="psql")
 
-def get_keys(dump):
-    keys = []
-    for key_ in dump:
-        if isinstance(key_, dict) and key_:
-            keys.append(list(key_.keys())[0])
-        else:
-            keys.append(key_)
-    return keys
- 
+
 # print dump
 def print_dump(collected_info, table, module, identifier, key_map):
     if not table:
@@ -184,7 +194,7 @@ def print_dump(collected_info, table, module, identifier, key_map):
                     tabulate_fmt.append([tab])
                 total_info += tabulate(tabulate_fmt, ["Tables Not Found"], tablefmt="grid")
                 total_info += "\n"
-    
+
             if not key_map:
                 values = []
                 hdrs = ["Keys", "field-value pairs"]
@@ -204,7 +214,7 @@ def print_dump(collected_info, table, module, identifier, key_map):
                 temp = []
                 for pair in collected_info[ids][db]["vidtorid"].items():
                     temp.append(list(pair))
-                total_info +=str(tabulate(temp, headers=["vid", "rid"], tablefmt="grid"))
+                total_info += str(tabulate(temp, headers=["vid", "rid"], tablefmt="grid"))
             final_collection.append([ids, db, total_info])
 
     click.echo(tabulate(final_collection, top_header, tablefmt="grid"))
--- tests/dump_tests/dump_state_test.py
+++ tests/dump_tests/dump_state_test.py
@@ -8,8 +8,9 @@ from deepdiff import DeepDiff
 from importlib import reload
 from utilities_common.db import Db
 import traceback
-from .module_tests import mock_sonicv2connector
 from utilities_common.constants import DEFAULT_NAMESPACE
+from .module_tests import mock_sonicv2connector
+
 
 def compare_json_output(exp_json, rec, exclude_paths=None):
     print("EXPECTED: \n")
@@ -18,8 +19,9 @@ def compare_json_output(exp_json, rec, exclude_paths=None):
         rec_json = json.loads(rec)
     except Exception as e:
         print(rec)
-        assert 0 , "CLI Output is not in JSON Format"   
+        assert False, "CLI Output is not in JSON Format"   
     return DeepDiff(exp_json, rec_json, exclude_paths=exclude_paths)
+
 
 table_display_output = '''\
 +-------------+-----------+----------------------------------------------------------------------------+
@@ -36,6 +38,7 @@ table_display_output = '''\
 |             |           | +----------------------+-------------------------------------------------+ |
 +-------------+-----------+----------------------------------------------------------------------------+
 '''
+
 
 table_display_output_no_filtering= '''\
 +-------------+-----------+-----------------------------------------------------------+
@@ -76,15 +79,15 @@ table_display_output_no_filtering= '''\
 +-------------+-----------+-----------------------------------------------------------+
 '''
 
+
 class TestDumpState(object):
-    
+
     @classmethod
     def setup_class(cls):
         print("SETUP")
         os.environ["UTILITIES_UNIT_TESTING"] = "1"
         mock_db_path = os.path.join(os.path.dirname(__file__), "../mock_tables/")
 
-    
     def test_identifier_single(self):
         runner = CliRunner()
         result = runner.invoke(dump.state, ["port", "Ethernet0"])
@@ -99,12 +102,12 @@ class TestDumpState(object):
         pths = ["root['Ethernet0']['CONFIG_DB']['keys'][0]['PORT|Ethernet0']", "root['Ethernet0']['APPL_DB']['keys'][0]['PORT_TABLE:Ethernet0']"]
         ddiff = compare_json_output(expected, result.output, exclude_paths = pths)
         assert not ddiff, ddiff
-        
+
     def test_identifier_multiple(self):
         runner = CliRunner()
         result = runner.invoke(dump.state, ["port", "Ethernet0,Ethernet4"])
         print(result.output)
-        expected= {"Ethernet0": 
+        expected = {"Ethernet0":
                    {"CONFIG_DB":{"keys":[{"PORT|Ethernet0":{"alias":"etp1","description":"etp1","index":"0","lanes":"25,26,27,28","mtu":"9100","pfc_asym":"off","speed":"40000"}}],"tables_not_found":[]},
                     "APPL_DB":{"keys":[{"PORT_TABLE:Ethernet0":{"index":"0","lanes":"0","alias":"Ethernet0","description":"ARISTA01T2:Ethernet1","speed":"25000","oper_status":"down","pfc_asym":"off","mtu":"9100","fec":"rs","admin_status":"up"}}],"tables_not_found":[]},
                     "ASIC_DB":{"keys":[{"ASIC_STATE:SAI_OBJECT_TYPE_HOSTIF:oid:0xd00000000056d":{"SAI_HOSTIF_ATTR_NAME":"Ethernet0","SAI_HOSTIF_ATTR_OBJ_ID":"oid:0x10000000004a4","SAI_HOSTIF_ATTR_OPER_STATUS":"true","SAI_HOSTIF_ATTR_TYPE":"SAI_HOSTIF_TYPE_NETDEV","SAI_HOSTIF_ATTR_VLAN_TAG":"SAI_HOSTIF_VLAN_TAG_STRIP"}},{"ASIC_STATE:SAI_OBJECT_TYPE_PORT:oid:0x10000000004a4":{"NULL":"NULL","SAI_PORT_ATTR_ADMIN_STATE":"true","SAI_PORT_ATTR_MTU":"9122","SAI_PORT_ATTR_SPEED":"100000"}}],"tables_not_found":[],"vidtorid":{"oid:0xd00000000056d":"oid:0xd","oid:0x10000000004a4":"oid:0x1690000000001"}},
@@ -119,7 +122,7 @@ class TestDumpState(object):
         pths.extend(["root['Ethernet4']['CONFIG_DB']['keys'][0]['PORT|Ethernet4']", "root['Ethernet4']['APPL_DB']['keys'][0]['PORT_TABLE:Ethernet4']"])
         ddiff = compare_json_output(expected, result.output, pths)
         assert not ddiff, ddiff
-    
+
     def test_option_key_map(self):
         runner = CliRunner()
         result = runner.invoke(dump.state, ["port", "Ethernet0", "--key-map"])
@@ -131,7 +134,7 @@ class TestDumpState(object):
         assert result.exit_code == 0, "exit code: {}, Exception: {}, Traceback: {}".format(result.exit_code, result.exception, result.exc_info)
         ddiff = compare_json_output(expected, result.output)
         assert not ddiff, ddiff
-        
+
     def test_option_db_filtering(self):
         runner = CliRunner()
         result = runner.invoke(dump.state, ["port", "Ethernet0", "--db", "ASIC_DB", "--db", "STATE_DB"])
@@ -141,7 +144,7 @@ class TestDumpState(object):
         assert result.exit_code == 0, "exit code: {}, Exception: {}, Traceback: {}".format(result.exit_code, result.exception, result.exc_info)
         ddiff = compare_json_output(expected, result.output)
         assert not ddiff, ddiff
-    
+
     def test_option_tabular_display(self):
         runner = CliRunner()
         result = runner.invoke(dump.state, ["port", "Ethernet0", "--db", "STATE_DB", "--table"])
@@ -155,7 +158,7 @@ class TestDumpState(object):
         print(result.output)
         assert result.exit_code == 0, "exit code: {}, Exception: {}, Traceback: {}".format(result.exit_code, result.exception, result.exc_info)
         assert table_display_output_no_filtering == result.output
-    
+
     def test_identifier_all_with_filtering(self):
         runner = CliRunner()
         expected_entries = []
@@ -169,20 +172,21 @@ class TestDumpState(object):
             assert 0 , "CLI Output is not in JSON Format" 
         ddiff = DeepDiff(set(expected_entries), set(rec_json.keys()))
         assert not ddiff, "Expected Entries were not recieved when passing all keyword"
-    
+
     def test_namespace_single_asic(self):
         runner = CliRunner()
         result = runner.invoke(dump.state, ["port", "Ethernet0", "--table", "--key-map", "--namespace", "asic0"])
         print(result.output)
         assert result.output == "Namespace option is not valid for a single-ASIC device\n"
-    
+
     @classmethod
     def teardown(cls):
         print("TEARDOWN")
         os.environ["UTILITIES_UNIT_TESTING"] = "0"
-    
+
+
 class TestDumpStateMultiAsic(object):
-    
+
     @classmethod
     def setup_class(cls):
         print("SETUP")
@@ -192,7 +196,7 @@ class TestDumpStateMultiAsic(object):
         reload(mock_multi_asic)
         from ..mock_tables import dbconnector
         dbconnector.load_namespace_config()
-        
+
     def test_default_namespace(self):
         runner = CliRunner()
         db = Db()
@@ -204,7 +208,7 @@ class TestDumpStateMultiAsic(object):
         assert result.exit_code == 0, "exit code: {}, Exception: {}, Output: {}".format(result.exit_code, result.exception, result.output)
         ddiff = compare_json_output(expected, result.output)
         assert not ddiff, ddiff
-    
+
     def test_namespace_asic0(self):
         runner = CliRunner()
         db = Db()
@@ -217,7 +221,6 @@ class TestDumpStateMultiAsic(object):
         ddiff = compare_json_output(expected, result.output)
         assert not ddiff, ddiff
 
-    
     def test_namespace_asic1(self):
         runner = CliRunner()
         db = Db()
@@ -230,13 +233,12 @@ class TestDumpStateMultiAsic(object):
         assert result.exit_code == 0, "exit code: {}, Exception: {}, Output: {}".format(result.exit_code, result.exception, result.output)
         ddiff = compare_json_output(expected, result.output)
         assert not ddiff, ddiff
-    
+
     def test_invalid_namespace(self):
         runner = CliRunner()
         db = Db()
         result = runner.invoke(dump.state, ["port", "Ethernet0", "--namespace", "asic3"], obj=db)
         assert result.output == "Namespace option is not valid. Choose one of ['asic0', 'asic1']\n", result
-    
 
     def teardown_class(cls):
         print("TEARDOWN")
