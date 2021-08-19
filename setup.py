556756b410d4d1ac64f1dd3361945d0fc3ccb484
Vivek Reddy Karri <vkarri@nvidia.com>
2021-08-19 03:27:32 +0000

    setup.py updated
    Signed-off-by: Vivek Reddy Karri <vkarri@nvidia.com>

--- setup.py
+++ setup.py
@@ -5,21 +5,9 @@
 # under scripts/. Consider stop using scripts and use console_scripts instead
 #
 # https://stackoverflow.com/questions/18787036/difference-between-entry-points-console-scripts-and-scripts-in-setup-py
-import fastentrypoints, sys
+import fastentrypoints
 
 from setuptools import setup
-from setuptools.command.test import test as TestCommand
-
-class PyTest(TestCommand):
-    user_options = [("pytest-args=", "a", "Arguments to pass to pytest")]
-    def initialize_options(self):
-        TestCommand.initialize_options(self)
-        self.pytest_args = ""
-    def run_tests(self):
-        import shlex
-        import pytest
-        errno = pytest.main(shlex.split(self.pytest_args))
-        sys.exit(errno)
 
 setup(
     name='sonic-utilities',
@@ -232,6 +220,5 @@ setup(
         'Topic :: Utilities',
     ],
     keywords='sonic SONiC utilities command line cli CLI',
-    cmdclass={"pytest": PyTest},
     test_suite='setup.get_test_suite'
 )
