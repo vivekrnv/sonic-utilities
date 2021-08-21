d2ebee838fd02d2efdb752aa51a4b2dcf5698286
Vivek Reddy Karri <vkarri@nvidia.com>
2021-08-21 02:48:39 +0000

    Changed the UT to use the default mock
    Signed-off-by: Vivek Reddy Karri <vkarri@nvidia.com>

--- dump/plugins/portchannel.py
+++ dump/plugins/portchannel.py
@@ -1,30 +1,31 @@
-from .executor import Executor
-from dump.match_infra import MatchEngine, MatchRequest
+from dump.match_infra import MatchRequest
 from dump.helper import create_template_dict, verbose_print
+from .executor import Executor
+
 
 class PortChannel(Executor):
     """
     Debug Dump Plugin for PortChannel/LAG Module
     """
     ARG_NAME = "portchannel_name"
-    
-    def __init__(self):
-        self.match_engine = MatchEngine()
+
+    def __init__(self, match_engine=None):
+        super().__init__(match_engine)
         self.ret_temp = {}
         self.ns = ''
         self.lag_members = set()
-          
+
     def get_all_args(self, ns=""):
         req = MatchRequest(db="CONFIG_DB", table="PORTCHANNEL", key_pattern="*", ns=ns)
         ret = self.match_engine.fetch(req)
         all_lags = ret["keys"]
         return [key.split("|")[-1] for key in all_lags]
-    
+
     def execute(self, params_dict):
         self.ret_temp = create_template_dict(dbs=["CONFIG_DB", "APPL_DB", "ASIC_DB", "STATE_DB"])
         self.lag_name = params_dict[PortChannel.ARG_NAME]
         self.ns = params_dict["namespace"]
-        # CONFIG_DB 
+        # CONFIG_DB
         lag_found = self.init_lag_config_info()
         if lag_found:
             self.init_lag_member_config_info()
@@ -40,7 +41,7 @@ class PortChannel(Executor):
         lag_type_objs_asic = self.init_lag_member_type_obj_asic_info()
         self.init_lag_asic_info(lag_type_objs_asic)
         return self.ret_temp
-    
+
     def add_to_ret_template(self, table, db, keys, err):
         if not err and keys:
             self.ret_temp[db]["keys"].extend(keys)
@@ -48,66 +49,66 @@ class PortChannel(Executor):
         else:
             self.ret_temp[db]["tables_not_found"].extend([table])
             return False
-    
+
     def init_lag_config_info(self):
         req = MatchRequest(db="CONFIG_DB", table="PORTCHANNEL", key_pattern=self.lag_name, ns=self.ns)
         ret = self.match_engine.fetch(req)
         return self.add_to_ret_template(req.table, req.db, ret["keys"], ret["error"])
-        
+
     def init_lag_member_config_info(self):
-        req = MatchRequest(db="CONFIG_DB", table="PORTCHANNEL_MEMBER", key_pattern=self.lag_name+"|*", ns=self.ns)
+        req = MatchRequest(db="CONFIG_DB", table="PORTCHANNEL_MEMBER", key_pattern=self.lag_name + "|*", ns=self.ns)
         ret = self.match_engine.fetch(req)
         for key in ret["keys"]:
             self.lag_members.add(key.split("|")[-1])
         self.add_to_ret_template(req.table, req.db, ret["keys"], ret["error"])
-    
+
     def init_lag_appl_info(self):
         req = MatchRequest(db="APPL_DB", table="LAG_TABLE", key_pattern=self.lag_name, ns=self.ns)
         ret = self.match_engine.fetch(req)
         return self.add_to_ret_template(req.table, req.db, ret["keys"], ret["error"])
-    
+
     def init_lag_member_appl_info(self):
-        req = MatchRequest(db="APPL_DB", table="LAG_MEMBER_TABLE", key_pattern=self.lag_name+":*", ns=self.ns)
+        req = MatchRequest(db="APPL_DB", table="LAG_MEMBER_TABLE", key_pattern=self.lag_name + ":*", ns=self.ns)
         ret = self.match_engine.fetch(req)
         for key in ret["keys"]:
             self.lag_members.add(key.split(":")[-1])
         self.add_to_ret_template(req.table, req.db, ret["keys"], ret["error"])
-        
+
     def init_lag_state_info(self):
         req = MatchRequest(db="STATE_DB", table="LAG_TABLE", key_pattern=self.lag_name, ns=self.ns)
         ret = self.match_engine.fetch(req)
         return self.add_to_ret_template(req.table, req.db, ret["keys"], ret["error"])
-    
+
     def init_lag_member_state_info(self):
-        req = MatchRequest(db="STATE_DB", table="LAG_MEMBER_TABLE", key_pattern=self.lag_name+"|*", ns=self.ns)
+        req = MatchRequest(db="STATE_DB", table="LAG_MEMBER_TABLE", key_pattern=self.lag_name + "|*", ns=self.ns)
         ret = self.match_engine.fetch(req)
         for key in ret["keys"]:
             self.lag_members.add(key.split("|")[-1])
         self.add_to_ret_template(req.table, req.db, ret["keys"], ret["error"])
-    
+
     def init_lag_asic_info(self, lag_type_objs_asic):
         if len(lag_type_objs_asic) == 0:
             self.ret_temp["ASIC_DB"]["tables_not_found"].extend(["ASIC_STATE:SAI_OBJECT_TYPE_LAG"])
-            return 
+            return
         for lag_asic_obj in lag_type_objs_asic:
             req = MatchRequest(db="ASIC_DB", table="ASIC_STATE:SAI_OBJECT_TYPE_LAG", key_pattern=lag_asic_obj, ns=self.ns)
             ret = self.match_engine.fetch(req)
             self.add_to_ret_template(req.table, req.db, ret["keys"], ret["error"])
-    
+
     def init_lag_member_type_obj_asic_info(self):
         lag_type_objs_asic = set()
         for port_name in self.lag_members:
             port_asic_obj = self.get_port_asic_obj(port_name)
             if port_asic_obj:
                 lag_member_key, lag_oid = self.get_lag_and_member_obj(port_asic_obj)
-                self.add_to_ret_template("ASIC_STATE:SAI_OBJECT_TYPE_LAG_MEMBER" , "ASIC_DB", [lag_member_key], "")
+                self.add_to_ret_template("ASIC_STATE:SAI_OBJECT_TYPE_LAG_MEMBER", "ASIC_DB", [lag_member_key], "")
                 lag_type_objs_asic.add(lag_oid)
         if not lag_type_objs_asic:
             self.ret_temp["ASIC_DB"]["tables_not_found"].extend(["ASIC_STATE:SAI_OBJECT_TYPE_LAG_MEMBER"])
         return lag_type_objs_asic
-             
+
     def get_port_asic_obj(self, port_name):
-        req = MatchRequest(db="ASIC_DB", table="ASIC_STATE:SAI_OBJECT_TYPE_HOSTIF", key_pattern="*", field="SAI_HOSTIF_ATTR_NAME", 
+        req = MatchRequest(db="ASIC_DB", table="ASIC_STATE:SAI_OBJECT_TYPE_HOSTIF", key_pattern="*", field="SAI_HOSTIF_ATTR_NAME",
                            value=port_name, return_fields=["SAI_HOSTIF_ATTR_OBJ_ID"], ns=self.ns)
         ret = self.match_engine.fetch(req)
         asic_port_obj_id = ""
@@ -116,9 +117,9 @@ class PortChannel(Executor):
             if sai_hostif_obj_key in ret["return_values"] and "SAI_HOSTIF_ATTR_OBJ_ID" in ret["return_values"][sai_hostif_obj_key]:
                 asic_port_obj_id = ret["return_values"][sai_hostif_obj_key]["SAI_HOSTIF_ATTR_OBJ_ID"]
         return asic_port_obj_id
-    
+
     def get_lag_and_member_obj(self, port_asic_obj):
-        req = MatchRequest(db="ASIC_DB", table="ASIC_STATE:SAI_OBJECT_TYPE_LAG_MEMBER", key_pattern="*", field="SAI_LAG_MEMBER_ATTR_PORT_ID", 
+        req = MatchRequest(db="ASIC_DB", table="ASIC_STATE:SAI_OBJECT_TYPE_LAG_MEMBER", key_pattern="*", field="SAI_LAG_MEMBER_ATTR_PORT_ID",
                            value=port_asic_obj, return_fields=["SAI_LAG_MEMBER_ATTR_LAG_ID"], ns=self.ns)
         ret = self.match_engine.fetch(req)
         lag_member_key = ""
--- tests/dump_tests/module_tests/portchannel_test.py
+++ tests/dump_tests/module_tests/portchannel_test.py
@@ -6,53 +6,85 @@ from deepdiff import DeepDiff
 from mock import patch
 from dump.helper import create_template_dict, sort_lists
 from dump.plugins.portchannel import PortChannel
+from dump.match_infra import MatchEngine, ConnectionPool
+from swsscommon.swsscommon import SonicV2Connector
 
-from .mock_sonicv2connector import MockSonicV2Connector
 
+# Location for dedicated db's used for UT
 module_tests_path = os.path.dirname(__file__)
 dump_tests_path = os.path.join(module_tests_path, "../")
 tests_path = os.path.join(dump_tests_path, "../")
 dump_test_input = os.path.join(tests_path, "dump_input")
-
-
-# Location for dedicated db's used for UT
 port_files_path = os.path.join(dump_test_input, "portchannel")
 
+# Define the mock files to read from
 dedicated_dbs = {}
 dedicated_dbs['CONFIG_DB'] = os.path.join(port_files_path, "config_db.json") 
 dedicated_dbs['APPL_DB'] = os.path.join(port_files_path, "appl_db.json") 
 dedicated_dbs['ASIC_DB'] = os.path.join(port_files_path, "asic_db.json")
 dedicated_dbs['STATE_DB'] = os.path.join(port_files_path, "state_db.json")
 
-def mock_connector(namespace, use_unix_socket_path=True):
-    return MockSonicV2Connector(dedicated_dbs, namespace=namespace, use_unix_socket_path=use_unix_socket_path)
 
-@pytest.fixture(scope="module", autouse=True)
-def verbosity_setup():
+def populate_mock(db, db_names):
+    for db_name in db_names:
+        db.connect(db_name)
+        # Delete any default data
+        db.delete_all_by_pattern(db_name, "*")
+        with open(dedicated_dbs[db_name]) as f:
+            mock_json = json.load(f)
+        for key in mock_json:
+            for field, value in mock_json[key].items():
+                db.set(db_name, key, field, value)
+
+
+@pytest.fixture(scope="class", autouse=True)
+def match_engine():
+
     print("SETUP")
     os.environ["VERBOSE"] = "1"
-    yield
+
+    # Monkey Patch the SonicV2Connector Object
+    from ...mock_tables import dbconnector
+    db = SonicV2Connector()
+
+    # popualate the db with mock data
+    db_names = list(dedicated_dbs.keys())
+    try:
+        populate_mock(db, db_names)
+    except Exception as e:
+        assert False, "Mock initialization failed: " + str(e)
+
+    # Initialize connection pool
+    conn_pool = ConnectionPool()
+    DEF_NS = ''  # Default Namespace
+    conn_pool.cache = {DEF_NS: {'conn': db,
+                               'connected_to': set(db_names)}}
+
+    # Initialize match_engine
+    match_engine = MatchEngine(conn_pool)
+    yield match_engine
     print("TEARDOWN")
     os.environ["VERBOSE"] = "0"
 
-@patch("dump.match_infra.SonicV2Connector", mock_connector)
-class TestPortChannelModule(unittest.TestCase):
-    def test_get_all_args(self):
+
+@pytest.mark.usefixtures("match_engine")
+class TestPortChannelModule:
+    def test_get_all_args(self, match_engine):
         """
         Scenario: Verify Whether the get_all_args method is working as expected
         """
-        m_lag = PortChannel()
+        m_lag = PortChannel(match_engine)
         returned = m_lag.get_all_args("")
         expect = ["PortChannel001", "PortChannel002", "PortChannel003"]
         ddiff = DeepDiff(expect, returned, ignore_order=True)
-        assert not ddiff, ddiff 
-    
-    def test_missing_appl_state(self):
+        assert not ddiff, ddiff
+
+    def test_missing_appl_state(self, match_engine):
         '''
         Scenario: When the LAG is configured but the Change is not propagated
         '''
         params = {PortChannel.ARG_NAME:"PortChannel003", "namespace":""}
-        m_lag = PortChannel()
+        m_lag = PortChannel(match_engine)
         returned = m_lag.execute(params)
         expect = create_template_dict(dbs=["CONFIG_DB", "APPL_DB", "ASIC_DB", "STATE_DB"])
         expect["CONFIG_DB"]["keys"].append("PORTCHANNEL|PortChannel003")
@@ -65,14 +97,14 @@ class TestPortChannelModule(unittest.TestCase):
         expect["ASIC_DB"]["tables_not_found"].append("ASIC_STATE:SAI_OBJECT_TYPE_LAG_MEMBER")
         ddiff = DeepDiff(returned, expect, ignore_order=True)
         assert not ddiff, ddiff
-    
-    def test_lag_with_no_members(self):
+
+    def test_lag_with_no_members(self, match_engine):
         '''
         Scenario: When the PortChannel doesn't have any members, 
                   it is not possible to uniquely identify ASIC LAG Related Key
         '''
         params = {PortChannel.ARG_NAME:"PortChannel002", "namespace":""}
-        m_lag = PortChannel()
+        m_lag = PortChannel(match_engine)
         returned = m_lag.execute(params)
         expect = create_template_dict(dbs=["CONFIG_DB", "APPL_DB", "ASIC_DB", "STATE_DB"])
         expect["CONFIG_DB"]["keys"].append("PORTCHANNEL|PortChannel002")
@@ -86,13 +118,13 @@ class TestPortChannelModule(unittest.TestCase):
         ddiff = DeepDiff(returned, expect, ignore_order=True)
         assert not ddiff, ddiff
 
-    def test_lag_with_members(self):
+    def test_lag_with_members(self, match_engine):
         '''
         Scenario: It should be possible to uniquely identify ASIC LAG Related Keys,
                   when the LAG has members
         '''
         params = {PortChannel.ARG_NAME:"PortChannel001", "namespace":""}
-        m_lag = PortChannel()
+        m_lag = PortChannel(match_engine)
         returned = m_lag.execute(params)
         expect = create_template_dict(dbs=["CONFIG_DB", "APPL_DB", "ASIC_DB", "STATE_DB"])
         expect["CONFIG_DB"]["keys"].append("PORTCHANNEL|PortChannel001")
@@ -107,5 +139,6 @@ class TestPortChannelModule(unittest.TestCase):
         expect["ASIC_DB"]["keys"].append("ASIC_STATE:SAI_OBJECT_TYPE_LAG:oid:0x2000000000d17")
         expect["ASIC_DB"]["keys"].append("ASIC_STATE:SAI_OBJECT_TYPE_LAG_MEMBER:oid:0x1b000000000d18")
         expect["ASIC_DB"]["keys"].append("ASIC_STATE:SAI_OBJECT_TYPE_LAG_MEMBER:oid:0x1b000000000d1a")
-        ddiff = DeepDiff(returned, expect, ignore_order=True)
+        ddiff = DeepDiff(expect, returned, ignore_order=True)
+        print(returned, expect, ddiff)
         assert not ddiff, ddiff
