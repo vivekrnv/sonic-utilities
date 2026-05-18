006b91f918a696bf0a3dae49e47a9db94f7472bd
Volodymyr Samotiy <volodymyrs@nvidia.com>
2026-05-18 22:48:23 +0300

    [Mellanox] [generate_dump] Collect SSD dump (#4508)
    Signed-off-by: Volodymyr Samotiy <volodymyrs@nvidia.com>

--- setup.py
+++ setup.py
@@ -89,6 +89,9 @@ setup(
         'watchdogutil',
         'sonic_cli_gen',
     ],
+    data_files=[
+        ("bin", ["ssd-dump/vtFA_RTK_5766_v2"]),
+    ],
     package_data={
         'generic_config_updater': ['gcu_services_validator.conf.json', 'gcu_field_operation_validators.conf.json'],
         'show': ['aliases.ini'],
@@ -139,6 +142,7 @@ setup(
         'scripts/flow_counters_stat',
         'scripts/gearboxutil',
         'scripts/generate_dump',
+        'scripts/generate_ssd_dump',
         'scripts/generate_shutdown_order.py',
         'scripts/intfutil',
         'scripts/intfstat',
